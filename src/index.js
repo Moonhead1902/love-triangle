8f7abf4bb76b03c34d10c65bc8fca4e4fb74ece8
Moonhead1902 <36646724+Moonhead1902@users.noreply.github.com>
2018-02-20 15:32:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var counter = 0;
+
+  for (var i = 0; i < preferences.length; i++){
+    if (preferences[i] != undefined){
+      var frstInt = preferences[i];
+      var scndInt = preferences[frstInt - 1];
+      var thrdInt = preferences[scndInt - 1];
+
+      if (i === thrdInt - 1 && frstInt != scndInt && scndInt != thrdInt){
+        counter++;
+        delete preferences[i];
+        delete preferences[frstInt - 1];
+        delete preferences[scndInt - 1];
+      }
+    }
+  }
+
+  return counter;
 };
